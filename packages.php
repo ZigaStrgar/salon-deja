5cf683af5b886b69d43ea2b9213234cdd56333d3
Žiga Strgar <ziga_strgar@hotmail.com>
2018-05-12 19:48:23 +0200

    Packages title change

--- packages.php
+++ packages.php
@@ -1,6 +1,6 @@
 <?php include_once "./header.php"; ?>
 <div class="block-flat">
-    <h1 class="page-header action text-center"> V našem salonu smo za Vas pripravili 5 stalnih paketov ugodnosti</h1>
+    <h1 class="page-header action text-center"> V našem salonu smo za Vas pripravili 6 stalnih paketov ugodnosti</h1>
     <div class="col-lg-12">
         <img src="./assets/img/packages.jpg" class="center-blockheader img-responsive" alt="Stalni paketi ugodnosti">
     </div>
